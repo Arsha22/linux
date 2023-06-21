246c7d9d2905b13f8ab8a01f7330953a64a0830d
Arsha22 <117925412+Arsha22@users.noreply.github.com>
2023-06-21 10:25:13 +0530

    Add files via upload

---
+++ count.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+getfile()
+{
+echo "enter the filename"
+read filename
+if  [ !  -f  $filename  ];then  
+          echo  "File Name  $filename does not exists."
+          exit 1
+      fi
+}
+clear
+getfile
+echo "converting Upper-case to Lower-case"
+tr  '[aA-Z]'  '[a-z]' <$filename
+echo "the number of lines is:"
+wc -l <$filename
+echo "the number of words are:"
+wc -w <$filename
+echo "the number of characters are:"
+wc -c <$filename
+echo "the content in descending order:"
+sort -r <$filename
